508682d0a56641e2ae135e19486ee458cf8549f2
peterzky <378096232@qq.com>
2017-02-23 00:30:06 +0800

    cleanup

--- xmonad.hs
+++ xmonad.hs
@@ -1,38 +1,39 @@
 
-import XMonad
+import           XMonad
 
-import System.IO 
-import System.Exit
+import           System.Exit
+import           System.IO
 
-import Data.Monoid
-import Data.List                        -- provide isInfixOf 
+import           Data.List
+-- import           Data.Monoid
 
-import XMonad.Hooks.DynamicLog          -- xmobar
-import XMonad.Hooks.ManageDocks         -- provide avoidstruts
-import XMonad.Hooks.EwmhDesktops 
-import XMonad.Hooks.ManageHelpers       -- provide isDialog
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.EwmhDesktops
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Hooks.ManageHelpers
 
-import XMonad.Util.Cursor               -- setdefaultcursor
-import XMonad.Util.Run(spawnPipe)       -- spawnPipe
-import XMonad.Util.SpawnOnce            -- startup hook
-import XMonad.Util.NamedScratchpad           -- scratchpad
-import XMonad.Util.WorkspaceCompare     -- provide sortByxineramaRule
+import           XMonad.Util.Cursor
+import           XMonad.Util.NamedScratchpad
+import           XMonad.Util.Run                  (spawnPipe)
+-- import           XMonad.Util.SpawnOnce
+import           XMonad.Util.WorkspaceCompare
 
-import XMonad.Actions.UpdatePointer     -- cursor follow focus
-import XMonad.Actions.WindowGo          -- raise window by className
+import           XMonad.Actions.UpdatePointer
+-- import           XMonad.Actions.WindowGo
+import XMonad.Actions.CycleWS
 
-import XMonad.Layout.IndependentScreens -- multi-head setup provide withScreens
-import XMonad.Layout.Spacing            -- provide Smartspacing 
-import XMonad.Layout.Renamed            -- custom layout
+import           XMonad.Layout.IndependentScreens
+import           XMonad.Layout.Renamed
+import           XMonad.Layout.Spacing
 -- import XMonad.Layout.Fullscreen         -- fix fullscreen issue
 
-import XMonad.Prompt                    -- provide system menu evoke with pluse key
-import XMonad.Prompt.XMonad
-import XMonad.Prompt.Shell
+-- import           XMonad.Prompt
+-- import           XMonad.Prompt.Shell
+import           XMonad.Prompt.XMonad
 
-  
-import qualified XMonad.StackSet as W
-import qualified Data.Map        as M
+
+import qualified Data.Map                         as M
+import qualified XMonad.StackSet                  as W
 
 
 
@@ -43,7 +44,7 @@ systemPromptCmds = [
         ("Hibernate" , spawn "sudo systemctl hibernate"),
         ("Restart"   , restart "xmonad" True)
                    ]
-                   
+
 myScratchPads = [ NS "fileManager" "nautilus"
                   (className =? "Nautilus")
                   defaultFloating
@@ -51,7 +52,7 @@ myScratchPads = [ NS "fileManager" "nautilus"
                   (className =? "Nextcloud")
                   defaultFloating
                 , NS "music" "xfce4-terminal -T musicbox -x musicbox"
-                  (title =? "musicbox") 
+                  (title =? "musicbox")
                   (customFloating $ W.RationalRect (1/4) (1/4) (1/2) (1/2))
                 , NS "htop" "xfce4-terminal -T htop -x htop"
                   (title =? "htop")
@@ -63,7 +64,7 @@ myScratchPads = [ NS "fileManager" "nautilus"
                   (title =? "term")
                   (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
                 , NS "ncmpcpp" "xfce4-terminal -T ncmpcpp -x ncmpcpp"
-                  (title =? "ncmpcpp") 
+                  (title =? "ncmpcpp")
                   (customFloating $ W.RationalRect (1/4) (1/4) (1/2) (1/2))
                 , NS "weechat" "xfce4-terminal -T weechat -x weechat"
                   (title =? "weechat")
@@ -80,31 +81,35 @@ myScratchPads = [ NS "fileManager" "nautilus"
 
 
 
+myTerminal :: [Char]
 myTerminal          = "urxvtc"
+
+myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
+
 myBorderWidth       = 0
 myModMask           = mod4Mask
 myWorkspaces        = withScreens 3 ["1","2","3","4","5","6","7","8","9"]
 myLauncher          = "rofi -run-command \"/bin/zsh -i -c '{cmd}'\" -hide-scrollbar -font \"Bitstream Vera Sans Mono 12\" -show run"
- 
+
 -- Border colors for unfocused and focused windows, respectively.
 --
- 
+
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
      -- Basic
- 
+
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
     , ((modm,               xK_space ), spawn myLauncher)
     , ((modm              , xK_q     ), kill)
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
-    , ((modm,               xK_p     ), sendMessage NextLayout)
+    , ((modm,               xK_o     ), sendMessage NextLayout)
     , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
     , ((modm,               xK_n     ), refresh)
-    , ((modm,               xK_Tab   ), windows W.focusDown)
+    , ((modm,               xK_Tab   ), toggleWS)
     , ((modm,               xK_j     ), windows W.focusDown)
     , ((modm,               xK_k     ), windows W.focusUp  )
     , ((modm,               xK_m     ), windows W.focusMaster  )
@@ -118,8 +123,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
 
     -- Applications
-    , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
-    , ((modm .|. shiftMask, xK_r     ), spawn "killall xmobar; xmonad --recompile; xmonad --restart")
+    -- , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
+    -- , ((modm,               xK_r     ), spawn "urxvtc -e ranger")
+    , ((modm .|. shiftMask, xK_r      ), spawn "killall xmobar; xmonad --recompile; xmonad --restart")
     , ((0                 , xK_Pause  ), xmonadPromptC systemPromptCmds def)
     , ((modm              , xK_f      ), namedScratchpadAction myScratchPads "fileManager")
     , ((modm              , xK_e      ), namedScratchpadAction myScratchPads "music")
@@ -137,9 +143,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- Volume control
     , ((0                 , xK_F12   )
-      , spawn "pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo +3%")
+      , spawn "pactl set-sink-volume alsa_output.usb-Harman_Multimedia_JBL_Pebbles_1.0.0-00.analog-stereo +3%")
     , ((0                 , xK_F11   )
-      , spawn "pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo -3%")
+      , spawn "pactl set-sink-volume alsa_output.usb-Harman_Multimedia_JBL_Pebbles_1.0.0-00.analog-stereo -3%")
     , ((modm              , xK_F12   )
       , spawn "pactl set-sink-volume alsa_output.usb-Creative_Technology_Ltd_SB_X-Fi_Surround_5.1_Pro_000003XO-00.analog-stereo +3%")
     , ((modm              , xK_F11   )
@@ -147,10 +153,10 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- Screenshots
     , ((0                 , xK_Print )
-      , spawn "scrot -u ~/Sync/Screenshots/Screenshot%Y-%m-%d%H:%M:%S.png")
-    , ((modm              , xK_Print )
       , spawn "scrot -s ~/Sync/Screenshots/Screenshot%Y-%m-%d%H:%M:%S.png")
- 
+    , ((modm              , xK_Print )
+      , spawn "scrot -u ~/Sync/Screenshots/Screenshot%Y-%m-%d%H:%M:%S.png")
+
 
     ]
 
@@ -164,24 +170,24 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_s, xK_a, xK_d] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
- 
- 
+
+
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
 --
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
- 
+
     -- mod-button1, Set the window to floating mode and move by dragging
     [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
                                        >> windows W.shiftMaster))
- 
+
     -- mod-button2, Raise the window to the top of the stack
     , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
- 
+
     -- mod-button3, Set the window to floating mode and resize by dragging
     , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
                                        >> windows W.shiftMaster))
- 
+
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     -- set mouse side button to float and resize
     , ((0,    8), (\w -> focus w >> mouseMoveWindow w
@@ -191,17 +197,17 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
                                        >> windows W.shiftMaster))
 
     ]
- 
+
 ------------------------------------------------------------------------
 -- Layouts:
 
-myLayout = myTiled ||| myMirror ||| Full 
+myLayout = myTiled ||| myMirror ||| Full
 
   where
     myTiled = renamed [Replace "Tiled"] $ smartSpacing 2 $ Tall 1 (3/100) (1/2)
     myMirror = renamed [Replace "Mirror"] $ Mirror myTiled
-    
- 
+
+
 ------------------------------------------------------------------------
 -- Window rules:
 
@@ -223,14 +229,14 @@ myManageHook = composeAll . concat $
     myRFloats  = ["desktop_window"]
     myPTFloats = ["DownThemAll!","AutoProxy","Install user style"]
     myPCFloats = []
- 
+
 ------------------------------------------------------------------------
 -- Event handling
 
- 
+
 ------------------------------------------------------------------------
 -- Status bars and logging
- 
+
 xmobarTitleColor = "#3399ff"
 xmobarCurrentWorkspaceColor = "#CEFFAC"
 myNormalBorderColor  = "#7c7c7c"
@@ -242,24 +248,24 @@ myLogHook h = dynamicLogWithPP $ xmobarPP
             , ppTitle = xmobarColor xmobarTitleColor "" . shorten 50
             , ppSort = getSortByXineramaRule
             , ppLayout = xmobarColor "red" ""
-            } 
- 
+            }
+
 ------------------------------------------------------------------------
 -- Startup hook
- 
+
 myStartupHook =
       spawn "source ~/.fehbg"
-          <+> spawnOnce "compton -fcC"
+          <+> spawn "compton -fcC"
           <+> setDefaultCursor xC_left_ptr
-          <+> spawnOnce "sleep 1;xcape -e 'Control_L=Escape'"
-          <+> spawnOnce "fcitx"
-          <+> spawnOnce "urxvtd"
-          <+> spawnOnce "sogou-qimpanel"
-          <+> spawnOnce "emacs --daemon"
-          <+> spawnOnce "goldendict"
-          <+> spawnOnce "dunst"
+          -- <+> spawn "sleep 1;xcape -e 'Control_L=Escape'"
+          <+> spawn "fcitx"
+          <+> spawn "urxvtd"
+          <+> spawn "sogou-qimpanel"
+          <+> spawn "goldendict"
+          <+> spawn "dunst"
           <+> namedScratchpadAction myScratchPads "cloud"
- 
+          <+> spawn "systemctl --user start emacs.service"
+
 ------------------------------------------------------------------------
 
 
@@ -275,19 +281,19 @@ main = do
         , workspaces         = myWorkspaces
         , normalBorderColor  = myNormalBorderColor
         , focusedBorderColor = myFocusedBorderColor
- 
+
       -- key bindings
         , keys               = myKeys
         , mouseBindings      = myMouseBindings
- 
+
       -- hooks, layouts
         , layoutHook         = avoidStruts $  myLayout
         , handleEventHook    = mempty <+> docksEventHook <+> fullscreenEventHook
         , startupHook        = myStartupHook
-        , manageHook         = myManageHook 
+        , manageHook         = myManageHook
         , logHook            = myLogHook xmproc
                              >> updatePointer (0.9,0.9) (0.9,0.9)
           }
-    
- 
+
+
 --
