e0e9f2c60d3419da86abcbdd5396755094688c35
peterzky <peter.zky@qq.com>
2018-07-19 14:10:20 +0800

    use more general script path

--- xmonad.hs
+++ xmonad.hs
@@ -185,10 +185,10 @@ myKeys conf@XConfig {XMonad.modMask = modm} =
   , ((0, 0x1008FF13), spawn "pactl set-sink-volume @DEFAULT_SINK@ +2%")
   , ((0, 0x1008FF11), spawn "pactl set-sink-volume @DEFAULT_SINK@ -2%")
   , ((0, 0x1008FF12), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
-  , ((modm, xK_Home), spawn "~/.config/bspwm/scripts/AudioSwitch.sh speaker")
-  , ((modm, xK_End), spawn "~/.config/bspwm/scripts/AudioSwitch.sh headphone")
+  , ((modm, xK_Home), spawn "~/.bin/AudioSwitch.sh speaker")
+  , ((modm, xK_End), spawn "~/.bin/AudioSwitch.sh headphone")
    -- Screenshots
-  , ((0, xK_Print), spawn "$HOME/.config/bspwm/scripts/ScreenShot.sh")
+  , ((0, xK_Print), spawn "$HOME/.bin/ScreenShot.sh")
    -- System Prompt
    ,((0, xK_Pause), xmonadPromptC systemPromptCmds myPromptTheme)
    -- ,((modm, xK_slash), shiftToProjectPrompt)
